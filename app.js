c1c2d355e385286022000d048fe0c0459121f042
Shama-David Grier-Johnson <127712361+TheSecurityLead@users.noreply.github.com>
2023-11-20 16:24:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,16 @@ Product.rounds = 25;
 
 // Helper function to generate a random index
 function getRandomProduct() {
-  let index = Math.floor(Math.random() * Product.allProducts.length);
-  while (Product.lastShown.includes(index)) {
+  let index;
+  let counter = 0;
+  do {
     index = Math.floor(Math.random() * Product.allProducts.length);
-  }
+    counter++;
+    // If it's taking too long to find a non-duplicate, reset the lastShown array
+    if (counter > Product.allProducts.length * 2) {
+      Product.lastShown = [];
+    }
+  } while (Product.lastShown.includes(index));
   return index;
 }
 
@@ -27,12 +33,14 @@ function getRandomProduct() {
 function displayProducts() {
   let indices = [];
   while(indices.length < 3) {
-    let randomIndex = getRandomProduct(); 
+    let randomIndex = getRandomProduct();
     if(!indices.includes(randomIndex)) {
       indices.push(randomIndex);
     }
   }
 
+  Product.lastShown = indices;
+  
   let productSection = document.getElementById('product-section');
   productSection.innerHTML = '';
 
@@ -64,26 +72,51 @@ function handleProductClick(event) {
   }
 }
 
-// Function to show the results button
+// Function to display chart after voting is complete
+function displayChart() {
+  let ctx = document.getElementById('results-chart').getContext('2d');
+  let productNames = Product.allProducts.map(p => p.name);
+  let votes = Product.allProducts.map(p => p.timesClicked);
+  let views = Product.allProducts.map(p => p.timesShown);
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: productNames,
+      datasets: [{
+        label: 'Votes',
+        data: votes,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }, {
+        label: 'Views',
+        data: views,
+        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+        borderColor: 'rgba(255, 206, 86, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+// Modified showResultsButton function to include chart display
 function showResultsButton() {
   let button = document.createElement('button');
   button.textContent = 'View Results';
-  button.addEventListener('click', displayResults);
+  button.addEventListener('click', function() {
+    displayResults();
+    displayChart(); // Call displayChart when results are shown
+  });
   document.body.appendChild(button);
 }
-
-// Function to display voting results
-function displayResults() {
-  let resultsList = document.getElementById('results-list');
-  resultsList.innerHTML = '';
-  
-  for (let product of Product.allProducts) {
-    let listItem = document.createElement('li');
-    listItem.textContent = `${product.name} had ${product.timesClicked} votes, and was seen ${product.timesShown} times.`;
-    resultsList.appendChild(listItem);
-  }
-}
-
 // Instantiate product objects from image paths
 function createProducts() {
   let productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum',
@@ -107,6 +140,17 @@ function setup() {
       handleProductClick(event);
     }
   });
+}
+
+function displayResults() {
+  let resultsList = document.getElementById('results-list');
+  resultsList.innerHTML = ''; // Clear existing results
+
+  for (let product of Product.allProducts) {
+    let listItem = document.createElement('li');
+    listItem.textContent = `${product.name} had ${product.timesClicked} votes and was seen ${product.timesShown} times.`;
+    resultsList.appendChild(listItem);
+  }
 }
 
 // Call setup to initialize the app
